fc182099830c4c22ee17626a4313e1d4bc23d2d1
LucianoBaldoFagioli <lbfagioli@miuandes.cl>
2024-09-27 23:34:53 -0300

    fix: current_user is nil, so auth before

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
     @business = current_user.business
     @supplies = Supply.where(business_id: @business.id)
