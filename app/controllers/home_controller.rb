a6537301c680c8c47f63dc33caeeb0d47518e641
__token__ <lbfagioli@miuandes.cl>
2024-11-14 15:11:02 -0300

    setting sendgrid to send example mail

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,23 @@
+# using SendGrid's Ruby Library
+# https://github.com/sendgrid/sendgrid-ruby
+require 'sendgrid-ruby'
+include SendGrid
+
 class HomeController < ApplicationController
   def index
     @business = current_user.business
     @supplies = Supply.where(business_id: @business.id)
     @low_stock_supplies = @supplies.select { |supply| supply.stock <= (supply.requires + 1) }
+    from = Email.new(email: 'test@example.com')
+    to = Email.new(email: 'test@example.com')
+    subject = 'Sending with SendGrid is Fun'
+    content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+    mail = Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 end
